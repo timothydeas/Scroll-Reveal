f74e001a805532bcce8a5c22e6a04eaf3e738dd4
Tim <deasti@icloud.com>
2024-02-20 23:49:20 +0000

    test it out

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Add JavaScript below
+window.onload = function() {
+    ScrollReveal().reveal('.imgOne', {delay: 500});
+    ScrollReveal().reveal('.imgTwo', {delay: 1000});
+    ScrollReveal().reveal('.imgThree', {delay: 1500});
+    ScrollReveal().reveal('.imgFour', {delay: 2000});
+    ScrollReveal().reveal('.imgFive', {delay: 2500});
+};
