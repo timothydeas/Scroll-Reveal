e17492821d1860923001af968cef53d6a8e98a72
Tim <deasti@icloud.com>
2024-02-21 01:13:42 +0000

    final draft

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     ScrollReveal().reveal('.imgOne', {delay: 500});
-    ScrollReveal().reveal('.imgTwo', {delay: 1000});
-    ScrollReveal().reveal('.imgThree', {delay: 1500});
-    ScrollReveal().reveal('.imgFour', {delay: 2000});
-    ScrollReveal().reveal('.imgFive', {delay: 2500});
+    ScrollReveal().reveal('.imgTwo', {delay: 1500});
+    ScrollReveal().reveal('.imgThree', {delay: 2500});
+    ScrollReveal().reveal('.imgFour', {delay: 2750});
+    ScrollReveal().reveal('.imgFive', {delay: 3000});
 };
