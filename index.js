62b1a2180fd75a5c6d174e32fd76cc5ea941c049
Tim <deasti@icloud.com>
2024-02-23 04:25:42 +0000

    different reveal options

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 400});
-    ScrollReveal().reveal('.imgOne', {delay: 800});
-    ScrollReveal().reveal('.imgTwo', {delay: 1200});
-    ScrollReveal().reveal('.imgThree', {delay: 1500});
+    ScrollReveal().reveal('.imgOne', { easing: 'ease-in'});
+    ScrollReveal().reveal('.imgTwo', {interval: 600});
+    ScrollReveal().reveal('.imgThree', {distance: '0px', opacity: 0.8});
     ScrollReveal().reveal('.imgFour', {delay: 1700});
-    ScrollReveal().reveal('.imgFive', {delay: 2000});
+    ScrollReveal().reveal('.imgFive', {origin: 'bottom'});
 };
