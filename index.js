93797488c771dfcded29231962eca9f1aff4c967
Tim <deasti@icloud.com>
2024-02-23 01:49:54 +0000

    cleaning up load

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    ScrollReveal().reveal('header', {delay: 100});
     ScrollReveal().reveal('.imgOne', {delay: 500});
     ScrollReveal().reveal('.imgTwo', {delay: 1500});
     ScrollReveal().reveal('.imgThree', {delay: 2500});
