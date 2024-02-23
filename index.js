44acd44d3024fbcaa40dc544bb1a36dca441c5b6
Tim <deasti@icloud.com>
2024-02-23 04:57:23 +0000

    more tests

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 300});
-    ScrollReveal().reveal('.imgOne', {interval: 1000});
-    ScrollReveal().reveal('.imgTwo', {distance: '0px', opacity: 0.2});
+    ScrollReveal().reveal('.imgOne', {interval: 2000});
+    ScrollReveal().reveal('.imgTwo', {scale: 0.55});
     ScrollReveal().reveal('.imgThree', {rotate: {x: 20, z: 20}});
-    ScrollReveal().reveal('.imgFour', {scale: 0.65});
+    ScrollReveal().reveal('.imgFour', {distance: '0px', opacity: 0.2});
     ScrollReveal().reveal('.imgFive', {delay: 1700});
 };
