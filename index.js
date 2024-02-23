9b522136da0abb14fe53777364e6814c4560e659
Tim <deasti@icloud.com>
2024-02-23 05:24:59 +0000

    think I got it

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 300});
-    ScrollReveal().reveal('.imgOne', {interval: 3000});
+    ScrollReveal().reveal('.imgOne', {duration: 800});
     ScrollReveal().reveal('.imgTwo', {scale: 0.25});
     ScrollReveal().reveal('.imgThree', {rotate: {x: 20, z: 20}});
     ScrollReveal().reveal('.imgFour', {distance: '50px', opacity: 0.1});
-    ScrollReveal().reveal('.imgFive', {delay: 1500});
+    ScrollReveal().reveal('.imgFive', {delay: 1300});
 };
