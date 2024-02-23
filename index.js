006b07c6efeace2c3fbcc0133eed6688a4f32cf9
Tim <deasti@icloud.com>
2024-02-23 06:38:18 +0000

    final changes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 200});
 
     //reveal instances & options for the images:
-    ScrollReveal().reveal('#imgOne', {duration: 2000});
-    ScrollReveal().reveal('#imgTwo', {scale: 0.25});
+    ScrollReveal().reveal('#imgOne', {scale: 0.25});
+    ScrollReveal().reveal('#imgTwo', {duration: 2000});
     ScrollReveal().reveal('#imgThree', {rotate: {x: 20, z: 20}});
-    ScrollReveal().reveal('#imgFour', {distance: '50px', opacity: 0.1});
-    ScrollReveal().reveal('#imgFive', {delay: 1300});
+    ScrollReveal().reveal('#imgFour', {delay: 1300});
+    ScrollReveal().reveal('#imgFive', {distance: '50px', opacity: 0.1});
 };
