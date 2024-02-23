a83306f89495cae634ac6991e5b5e8a9065c0261
Tim <deasti@icloud.com>
2024-02-23 04:37:28 +0000

    testing changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-    ScrollReveal().reveal('.header', {duration: 400});
-    ScrollReveal().reveal('.imgOne', { easing: 'ease-in'});
-    ScrollReveal().reveal('.imgTwo', {interval: 600});
-    ScrollReveal().reveal('.imgThree', {distance: '0px', opacity: 0.8});
+    ScrollReveal().reveal('.header', {origin: 'bottom'});
+    ScrollReveal().reveal('.imgOne', {rotate: {x: 20, z: 20}});
+    ScrollReveal().reveal('.imgTwo', {interval: 700});
+    ScrollReveal().reveal('.imgThree', {distance: '0px', opacity: 0.5});
     ScrollReveal().reveal('.imgFour', {delay: 1700});
-    ScrollReveal().reveal('.imgFive', {origin: 'bottom'});
+    ScrollReveal().reveal('.imgFive', {duration: 600});
 };
