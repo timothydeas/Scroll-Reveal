ebff10c737218528f7f17ad71c2095b4f64b22e4
Tim <deasti@icloud.com>
2024-02-23 05:39:09 +0000

    changed to lists

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 300});
+    
+    //reveal instances & options for the images:
     ScrollReveal().reveal('.imgOne', {duration: 800});
     ScrollReveal().reveal('.imgTwo', {scale: 0.25});
     ScrollReveal().reveal('.imgThree', {rotate: {x: 20, z: 20}});
