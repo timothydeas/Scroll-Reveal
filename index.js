a3ef5393d1b6e270c4f8d82e33de03c839fccd39
Tim <deasti@icloud.com>
2024-02-23 02:45:27 +0000

    hidden header

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-    ScrollReveal().reveal('.imgOne', {delay: 500});
-    ScrollReveal().reveal('.imgTwo', {delay: 1500});
-    ScrollReveal().reveal('.imgThree', {delay: 2000});
-    ScrollReveal().reveal('.imgFour', {delay: 2100});
-    ScrollReveal().reveal('.imgFive', {delay: 2200});
+    ScrollReveal().reveal('.header', {delay: 1000});
+    ScrollReveal().reveal('.imgOne', {delay: 1100});
+    ScrollReveal().reveal('.imgTwo', {delay: 1200});
+    ScrollReveal().reveal('.imgThree', {delay: 1500});
+    ScrollReveal().reveal('.imgFour', {delay: 2000});
+    ScrollReveal().reveal('.imgFive', {delay: 2100});
 };
