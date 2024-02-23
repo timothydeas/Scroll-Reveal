3896f629c1627607f57d6a2c25b63fbd37c32ad4
Tim <deasti@icloud.com>
2024-02-23 02:47:08 +0000

    smaller delay

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    ScrollReveal().reveal('.header', {delay: 1000});
-    ScrollReveal().reveal('.imgOne', {delay: 1100});
+    ScrollReveal().reveal('.header', {delay: 400});
+    ScrollReveal().reveal('.imgOne', {delay: 800});
     ScrollReveal().reveal('.imgTwo', {delay: 1200});
     ScrollReveal().reveal('.imgThree', {delay: 1500});
     ScrollReveal().reveal('.imgFour', {delay: 2000});
