1ea7b4823e37b77fe50152d1119b3454e24fcd28
Tim <deasti@icloud.com>
2024-02-23 02:51:53 +0000

    even less time

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-    ScrollReveal().reveal('.header', {delay: 400});
+    ScrollReveal().reveal('.header', {delay: 200});
     ScrollReveal().reveal('.imgOne', {delay: 800});
     ScrollReveal().reveal('.imgTwo', {delay: 1200});
     ScrollReveal().reveal('.imgThree', {delay: 1500});
-    ScrollReveal().reveal('.imgFour', {delay: 2000});
-    ScrollReveal().reveal('.imgFive', {delay: 2100});
+    ScrollReveal().reveal('.imgFour', {delay: 1700});
+    ScrollReveal().reveal('.imgFive', {delay: 2000});
 };
