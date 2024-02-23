392fef7c3dacb7d344b1735f54ece717e9931bdd
Tim <deasti@icloud.com>
2024-02-23 05:20:27 +0000

    so many tweaks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     ScrollReveal().reveal('.header', {duration: 300});
-    ScrollReveal().reveal('.imgOne', {interval: 2000});
-    ScrollReveal().reveal('.imgTwo', {scale: 0.55});
+    ScrollReveal().reveal('.imgOne', {interval: 3000});
+    ScrollReveal().reveal('.imgTwo', {scale: 0.25});
     ScrollReveal().reveal('.imgThree', {rotate: {x: 20, z: 20}});
-    ScrollReveal().reveal('.imgFour', {distance: '0px', opacity: 0.2});
-    ScrollReveal().reveal('.imgFive', {delay: 1700});
+    ScrollReveal().reveal('.imgFour', {distance: '50px', opacity: 0.1});
+    ScrollReveal().reveal('.imgFive', {delay: 1500});
 };
